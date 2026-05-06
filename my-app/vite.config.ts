872ce479e768016435ddc786bf30b4f87dab77f8
Anthony Lin <linan@lafayette.edu>
2026-05-06 09:41:08 -0400

    Fix Vite API proxy port

--- my-app/vite.config.ts
+++ my-app/vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const apiProxyTarget = process.env.VITE_PROXY_API_TARGET ?? 'http://127.0.0.1:8000'
+const apiProxyTarget = process.env.VITE_PROXY_API_TARGET ?? 'http://127.0.0.1:8001'
 
 export default defineConfig({
   plugins: [react()],
